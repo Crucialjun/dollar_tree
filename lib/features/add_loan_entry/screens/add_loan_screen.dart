2fb13b873cc18e5117f660f7684a9457b156ec37
Nicholas Otieno <nich.otieno@gmail.com>
2023-02-26 10:32:05 +0300

    update

--- lib/features/add_loan_entry/screens/add_loan_screen.dart
+++ lib/features/add_loan_entry/screens/add_loan_screen.dart
@@ -6,10 +6,26 @@ class AddLoanScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Column(children: const [
-          Text("Add Loan"),
+        child: Column(children: [
+          const Text("Add Loan"),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Container(
+              width: size.width,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.black)),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(children: const [
+                  Icon(Icons.add_a_photo),
+                  Text("Add Photos"),
+                ]),
+              ),
+            ),
+          )
         ]),
       ),
     );
